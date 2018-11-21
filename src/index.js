21add1b5e302509e3d9ce03d5784cc9c12e2509d
Nathaniel Camomot <nathaniel.camomot@legalmatch.com>
2018-11-21 12:41:49 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const firebase = require("firebase");
-const { FirePeer } = require("firepeer");
+var firebase = require("firebase");
+var FirePeer = require("firepeer").FirePeer;
+var $ = require("jquery");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDl1bEJXXehzFKeAhIOd-VI6Sivial78iU",
@@ -10,55 +11,58 @@ firebase.initializeApp({
   messagingSenderId: "665543533969"
 });
 
-const auth = firebase.auth();
-const db = firebase.database();
-
-const ready = fn => {
-  if (
-    document.attachEvent
-      ? document.readyState === "complete"
-      : document.readyState !== "loading"
-  ) {
-    fn();
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
-};
-
-const init = () => {};
-ready(init);
-
-let firePeer;
-let vars = {
-  ctrl: {
-    connect: async function() {
-      const uid = jquery(this).text();
-      firePeer = new FirePeer();
-      const conn = await firePeer.connect(db.ref(`users/${uid}/connections`));
-      console.log(conn);
-    }
-  }
-};
-
-rivets.bind(jquery(".container"), vars);
-
-auth.onAuthStateChanged(user => {
-  if (user) {
-    vars.user = user;
-    const path = db.ref(`users/${user.uid}/connections`);
-    firePeer = new FirePeer(path);
-  } else {
-    if (firePeer) {
-      firePeer.destroy();
-    }
-  }
-});
-auth.signInAnonymously();
-
-db.ref(`users`).on("value", snapshot => {
-  const users = [];
-  snapshot.forEach(function(child) {
-    users.push(child.val());
+$(function() {
+  var firePeer = new FirePeer(firebase);
+  firePeer.on("connection", function(conn) {
+    conn.on("data", function(data) {
+      console.log(data.toString());
+    });
   });
-  vars.users = users;
+  var myUserId;
+
+  $("#users").on("click", ".user", function() {
+    const uid = $(this).text();
+    debugger;
+    firePeer
+      .connect(uid)
+      .then(function(conn) {
+        console.log(conn);
+        conn.send("hello");
+      })
+      .catch(console.error);
+  });
+
+  firebase
+    .auth()
+    .signInAnonymously()
+    .then(function(cred) {
+      myUserId = cred.user.uid;
+      $("#myUid").text(myUserId);
+
+      //presence
+      firebase
+        .database()
+        .ref("presence/" + myUserId)
+        .on("value", function(ss) {
+          if (ss && !ss.val()) {
+            ss.ref.onDisconnect().remove();
+            ss.ref.set(true);
+          }
+        });
+
+      // watch online users
+      firebase
+        .database()
+        .ref("presence")
+        .on("value", function(snaps) {
+          $("#users").html("");
+          if (snaps) {
+            snaps.forEach(function(ss) {
+              if (myUserId != ss.key) {
+                $("#users").append("<p class='user'>" + ss.key + "</p>");
+              }
+            });
+          }
+        });
+    });
 });
