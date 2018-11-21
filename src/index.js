ba9fc2c65fd624252002ea300237a43e43266890
Nathaniel Camomot <nathaniel.camomot@legalmatch.com>
2018-11-21 21:22:04 +0800

    save

--- src/index.js
+++ src/index.js
@@ -14,38 +14,40 @@ firebase.initializeApp({
 $(function() {
   var firePeer = new FirePeer(firebase, {
     allowOffer: function(offer) {
-      return confirm(offer.uid + " would like to chat");
+      return confirm($("#users ." + offer.uid).text() + " would like to chat");
     }
   });
   var name = prompt("Whats your display name?");
   $("#name").text(name);
 
   function newChat(conn) {
-    var parent = $("<div class='message-area'>").addClass(conn.id);
-    var messages = $("<div class='messages'>");
-    function newMessage(message) {
-      messages.append($("<p class='m'>").text(message));
-    }
-
-    var input = $("<input type='text'>").on("keypress", function(event) {
+    var name = $("#users ." + conn.uid).text();
+    var $parent = $("<div class='message-area'>").addClass(conn.uid);
+    var $messages = $("<div class='messages'>");
+    var $input = $("<input type='text'>").on("keypress", function(event) {
       if (event.keyCode == 13) {
-        var str = input.val();
-        newMessage(str);
+        var str = $input.val();
+        newMessage("you", str);
         conn.send(str);
-        input.val("");
+        $input.val("");
       }
     });
 
-    parent.append(messages).append(input);
-    $("#chats").append(parent);
+    function newMessage(sender, message) {
+      $messages.append($("<p class='m'>").text(sender + ": " + message));
+    }
+
+    newMessage("", "Connected to " + name);
+    $parent.append($messages).append($input);
+    $("#chats").append($parent);
 
     conn.on("data", function(data) {
-      newMessage(data.toString());
+      newMessage(name, data.toString());
     });
 
     conn.on("close", function() {
       console.log("close");
-      parent.remove();
+      $parent.remove();
     });
 
     conn.on("error", console.error);
@@ -78,14 +80,23 @@ $(function() {
           $("#users").html("");
           snaps.forEach(function(snap) {
             //if not you, then create a node
-            if (yourUid != snap.key) {
-              var user = $("<div class='user'>")
-                .addClass(snap.key)
-                .text(snap.val()) //show the name of the user
+            var uid = snap.key;
+            var name = snap.val();
+            if (yourUid != uid) {
+              var $user = $("<div class='user'>")
+                .addClass(uid)
+                .text(name) //show the name of the user
                 .click(function() {
-                  firePeer.connect(snap.key).catch(console.error);
+                  var $chat = $("#chats ." + uid);
+                  if ($chat.length) {
+                    //has chat already
+                    $("#chats .message-area").hide();
+                    $chat.show();
+                  } else {
+                    firePeer.connect(uid).catch(console.error);
+                  }
                 });
-              $("#users").append(user);
+              $("#users").append($user);
             }
           });
         });
