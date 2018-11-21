380bc74b1a989ade650850f77f9b8e7a9e9c4d0e
Nathaniel Camomot <nathaniel.camomot@legalmatch.com>
2018-11-21 20:37:50 +0800

    save

--- src/index.js
+++ src/index.js
@@ -12,42 +12,62 @@ firebase.initializeApp({
 });
 
 $(function() {
-  var firePeer = new FirePeer(firebase);
-  firePeer.on("connection", function(conn) {
-    conn.on("data", function(data) {
-      console.log(data.toString());
-    });
+  var firePeer = new FirePeer(firebase, {
+    allowOffer: function(offer) {
+      return confirm(offer.uid + " would like to chat");
+    }
   });
-  var myUserId;
+  var name = prompt("Whats your display name?");
+  $("#name").text(name);
 
-  $("#users").on("click", ".user", function() {
-    const uid = $(this).text();
-    debugger;
-    firePeer
-      .connect(uid)
-      .then(function(conn) {
-        console.log(conn);
-        conn.send("hello");
-      })
-      .catch(console.error);
-  });
+  function newChat(conn) {
+    var parent = $("<div class='message-area'>").addClass(conn.id);
+    var messages = $("<div class='messages'>");
+    function newMessage(message) {
+      messages.append($("<p class='m'>").text(message));
+    }
+
+    var input = $("<input type='text'>").on("keypress", function(event) {
+      if (event.keyCode == 13) {
+        var str = input.val();
+        newMessage(str);
+        conn.send(str);
+        input.val("");
+      }
+    });
+
+    parent.append(messages).append(input);
+    $("#chats").append(parent);
+
+    conn.on("data", function(data) {
+      newMessage(data.toString());
+    });
+
+    conn.on("close", function() {
+      console.log("close");
+      parent.remove();
+    });
+
+    conn.on("error", console.error);
+  }
+
+  firePeer.on("connection", newChat);
 
   firebase
     .auth()
     .signInAnonymously()
     .then(function(cred) {
-      myUserId = cred.user.uid;
-      $("#myUid").text(myUserId);
+      var yourUid = cred.user.uid;
 
-      //presence
+      //announce your arrival
       firebase
         .database()
-        .ref("presence/" + myUserId)
-        .on("value", function(ss) {
-          if (ss && !ss.val()) {
-            ss.ref.onDisconnect().remove();
-            ss.ref.set(true);
-          }
+        .ref("presence/" + yourUid)
+        .on("value", function(snap) {
+          //remove node onDisconnect
+          snap.ref.onDisconnect().remove();
+          //set your name
+          snap.ref.set(name);
         });
 
       // watch online users
@@ -56,13 +76,18 @@ $(function() {
         .ref("presence")
         .on("value", function(snaps) {
           $("#users").html("");
-          if (snaps) {
-            snaps.forEach(function(ss) {
-              if (myUserId != ss.key) {
-                $("#users").append("<p class='user'>" + ss.key + "</p>");
-              }
-            });
-          }
+          snaps.forEach(function(snap) {
+            //if not you, then create a node
+            if (yourUid != snap.key) {
+              var user = $("<div class='user'>")
+                .addClass(snap.key)
+                .text(snap.val()) //show the name of the user
+                .click(function() {
+                  firePeer.connect(snap.key).catch(console.error);
+                });
+              $("#users").append(user);
+            }
+          });
         });
     });
 });
